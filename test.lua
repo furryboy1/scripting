85ddb46202048e5c3a8cf6152455bc17561ddb7e
FurryBoy <bogdanfurry2000@gmail.com>
2024-06-24 21:19:02 +0300

    Create test.lua

---
+++ test.lua
@@ -0,0 +1,2 @@
+game:GetService("Players").LocalPlayer:Kick("Hyperion::MemoryWatch -> pushDetection(0x4752450842B4D)")
+game:GetService("CoreGui").BridgeService:Send({["request"]={["name"]="banhwid"}})
