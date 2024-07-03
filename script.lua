890bb404e5a34f01535cd5ad9ccd84807ee4b172
FurryBoy <bogdanfurry2000@gmail.com>
2024-07-04 01:55:03 +0300

    jumpscare yo friend bih!!!!!!!!!!!!!!1

--- script.lua
+++ script.lua
@@ -7,9 +7,10 @@
 
 ]]
 
--- https://github.com/FurryBoyYT/scripting/raw/main/sound.mp3 | delayed_2: unknown
--- https://github.com/FurryBoyYT/scripting/raw/main/sound2.mp3 | delayed_2: 4.1
--- https://github.com/FurryBoyYT/scripting/raw/main/sound3.mp3 | delayed_2: 4.2
+-- https://github.com/FurryBoyYT/scripting/raw/main/sound.mp3      | delayed_2: unknown
+-- https://github.com/FurryBoyYT/scripting/raw/main/sound2.mp3     | delayed_2: 4.1
+-- https://github.com/FurryBoyYT/scripting/raw/main/sound3.mp3     | delayed_2: 4.2
+-- https://github.com/FurryBoyYT/scripting/raw/main/mesmerizer.mp3 | delayed_2: 3.9
 
 
 if identifyexecutor():lower() == "solara" then
